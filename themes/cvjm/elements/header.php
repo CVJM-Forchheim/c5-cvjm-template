dce67fd723c86d1426e5a70b1f477e76b76eb705
Roland Greim <roland@stempelgreim.de>
2020-10-30 00:13:28 +0100

    x

--- themes/cvjm/elements/header.php
+++ themes/cvjm/elements/header.php
@@ -4,8 +4,7 @@
 <html lang = '<?= Localization::activeLanguage() ?>'>
 
 <head>
-< ?= Loader::element( 'header_required' );
-?>
+<?= Loader::element( 'header_required' );?>
 <meta http-equiv = 'X-UA-Compatible' content = 'IE=edge'>
 <meta name = 'viewport' content = 'width=device-width, initial-scale=1'>
 
